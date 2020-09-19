ecf0a4d89cebd59c57a178565492bc0097722daf
see-bet-code <brittani.elan.taylor@gmail.com>
2020-09-19 08:30:15 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(words_array)
+        words_array.select do |w|
+            w.split("").sort == @word.split("").sort
+        end
+    end
+end
